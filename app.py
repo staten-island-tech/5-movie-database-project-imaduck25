885438128ed610670909da9c0b328f5c8c3237fb
imaduck25 <fatimaa158@nycstudents.net>
2025-11-24 06:44:15 -0800

    Update app.py

--- app.py
+++ app.py
@@ -4,40 +4,47 @@ movies = open("./movies.json", encoding="utf8")
 ## create variable "data" that represents the enitre movie list
 data = json.load(movies)
 
-## File 1:
-for index, item in enumerate(data):
-    print(item["title"])
-
-""" ## File 2:
 def database():
-    year = int(input("enter a year: "))
-    print("movies released after", year,":")
-    for item in data:
-        if item["year"] > year:
-            print(item["title"])
+
+    """ ## File 1:
+    for index, item in enumerate(data):
+        print(item["title"])
+
+      ## File 2:
+    def after():
+        year = int(input("enter a year: "))
+        print("movies released after", year,":")
+        for item in data:
+            if item["year"] > year:
+                print(item["title"])
+    after()
+
+    ## File 3;
+    def before():
+        year = int(input("enter a year: "))
+        print("movies released b4", year,":")
+        for item in data:
+            if item["year"] < year:
+                print(item["title"])
+    before()
+
+    ## File 4:
+    def inyear():
+        year = int(input("enter a year: "))
+        print("movies released in", year,":")
+        for item in data:
+            if item["year"] == year:
+                print(item["title"])
+    inyear()
  """
-""" ## File 3;
-def database():
-    year = int(input("enter a year: "))
-    print("movies released b4", year,":")
-    for item in data:
-        if item["year"] < year:
-            print(item["title"])
-database()
-
-## File 4:
-def database():
-    year = int(input("enter a year: "))
-    print("movies released in", year,":")
-    for item in data:
-        if item["year"] == year:
-            print(item["title"])
-database() """
-
-## File 5:
-def database():
-    input("search for a movie: ")
-    if any(char for char in item["title"]) == input:
-        for data, item["title"] in enumerate(data):
-            print(item["title"])
+    ## File 5:
+    def search():
+        input("search for a movie: ")
+        if any(char for char in movies["title"]) == input:
+            for movies, movies["title"] in enumerate(data):
+                print(data["title"])
+    search()
+    
+    ## File 6:
+    
 database()
